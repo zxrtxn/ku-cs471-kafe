6a2f43d9b0fb9b8a5bc482cb76ba0f604e2daed8
Supakrit Pamakama <spk.zxrtxn@gmail.com>
2024-09-10 12:58:13 +0700

    add home page

---
+++ src/main/java/ku/cs/kafe/controller/HomeController.java
@@ -0,0 +1,15 @@
+package ku.cs.kafe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        model.addAttribute("greeting", "Sawaddee");
+        return "home";
+    }
+}
